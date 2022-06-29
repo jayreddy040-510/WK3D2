8507d7366955afa1ec027974e3ece31394e542d4
AA Student <appacademy@Mini-7.local>
2022-06-28 17:36:23 -0700

    Unfinishedhomework

--- MemoryPuzzle/board.rb
+++ MemoryPuzzle/board.rb
@@ -2,25 +2,29 @@ require "byebug"
 require_relative "card.rb"
 
 class Board
+    
 
-    def initialize(n)
-        @grid = Array.new(n) {Array.new(n,"")}
-        @size = n**2
+    def initialize
+        @grid = Array.new(4) {Array.new(4,"_")}
+        @size = 16
 
     end
 
+    
 
 
     def [](pos)
-        @grid[pos[0]][pos[1]]
+        row,col = pos
+        @grid[row][col]
     end
 
     def []=(pos, value)
-        @grid[pos[0]][pos[1]] = value
+        row,col = pos
+        @grid[row][col]= value
     end
 
     def empty?(pos)
-        if @grid[pos[0]][pos[1]] == ""
+        if self[pos] == "_"
             return true
         else
             return false
@@ -28,9 +32,9 @@ class Board
     end
 
     def empty_positions?
-        @grid.each do | row|
+        @grid.each do |row|
             row.each do |x|
-                if x == ""
+                if x == "_"
                     return true
                 end
             end
@@ -40,38 +44,71 @@ class Board
 
 
     def populate
-
-        card = Card.new("F", false)
-        deck = card.make_deck
-        deck.each do |el|
-            while self.empty_positions?
-                row = rand(0...@grid.length)
-                col = rand(0...@grid.length)
-                row2 = rand(0...@grid.length)
-                col2 = rand(0...@grid.length)
-                if (@grid[[row,col]].to_s == "") && (@grid[[row2,col]].to_s == "")  &&  (@grid[[row,col]] != @grid[[row2,col2]])
-                    @grid[row][col] = el
-                    @grid[row2][col2] = el
-                end
-            end
-                
+        alphas = ("A".."H").to_a.shuffle!
+        input = []
+        alphas.each do |char|
+            c1 = Card.new(char,false)
+            c2 = Card.new(char,false)
+            input<<c1<<c2
         end
     
+        @grid.each_with_index do |row, idx1|
+            row.each_with_index do |col, idx2|
+                pos = [idx1, idx2]
+                p = input.shuffle!
+                self[pos] = p.pop
+            end
+        end
+    end 
     
+    def render
+        @grid.each do |row|
+            row.each do |el|
+                el.display
+                
+
+            end
+            puts row.join(" ")
+        end
+            
     end
+    
+    
+end
+
+#     arr = ("A".."Z").to_a
+# self[pos] = arr.shuffle.pop
+#     def populate
+# #
+#         card = Card.new("F", false)
+#         deck = card.make_deck
+#         deck.each do |el|
+#             while self.empty_positions?
+#                 row = rand(0...@grid.length)
+#                 col = rand(0...@grid.length)
+#                 row2 = rand(0...@grid.length)
+#                 col2 = rand(0...@grid.length)
+#                 if (@grid[[row,col]].to_s == "") && (@grid[[row2,col]].to_s == "")  &&  (@grid[[row,col]] != @grid[[row2,col2]])
+#                     @grid[row][col] = el
+#                     @grid[row2][col2] = el
+#                 end
+#             end
+                
+#         end
+    
+    
+#     end
 
     def print
         @grid.each do |row|
-            row.each do |el|
-                if el
-                # el.reveal
-                end
-            end
+            # row.each do |el|
+                # if el
+                # # el.reveal
+                # end
+            # end
             puts row.join(" ")
         end
     end
         
 
 
-
-end
--- MemoryPuzzle/card.rb
+++ MemoryPuzzle/card.rb
@@ -1,55 +1,69 @@
 class Card
-    attr_accessor :face_value
+
+    attr_accessor :face_value, :face_up
     def initialize(face_value, face_up)
         @face_value = face_value
         @face_up = face_up
     end
 
-    def face_up
-        @face_up
-    end
-
-    def face_up=(value)
-        @face_up = value
-    end
-
-    def make_deck
-        deck = []
-        arr = ("A".."Z").to_a
-        arr.shuffle!
-        arr.each do |el|
-            card = Card.new(el, false)
-            deck << card
+    def display
+        if self.face_up == true
+            p self.face_value 
+        else
+            puts '_' 
         end
-        
-        deck
+    
     end
 
     def hide
-        if self.face_up == true
-            self.face_up = false
-            return true
-        else
-            return false
-        end
+        self.face_up = false
     end
 
     def reveal
-        if self.face_up == false
-            self.face_up = true
-            return true
-        else
-            return false
-        end
+        self.face_up = true
     end
 
     def to_s
         self.face_value.to_s
+    
     end
 
-    # def ==(sec_card)
-    #     self.face_up == sec_card.face_up
+    def ==(sec_card)
+        self.face_value == sec_card.face_value
+    
+    end
+
+    # def face_up
+    #     @face_up
     # end
+
+    # def face_up=(value)
+    #     @face_up = value
+    # end
+
+    # def make_deck
+    #     deck = []
+    #     arr = ("A".."Z").to_a
+    #     arr.shuffle!
+    #     arr.each do |el|
+    #         card = Card.new(el, false)
+    #         deck << card
+    #     end
+        
+    #     deck
+    # end
+
+
+
+
+
+    # def to_s
+    #     self.face_value
+    # end
+
+    # # def ==(sec_card)
+    # #     self.face_up == sec_card.face_up
+    # # end
 
     
 
